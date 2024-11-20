bbc0856bb6bae905496ca251bba20a13b309d515
Kasra Torshizi <ktorsh@umd.edu>
2024-11-20 12:33:23 -0500

    Remove duplicate function

--- image_to_grid_env.py
+++ image_to_grid_env.py
@@ -41,49 +41,6 @@ class ImageToGrid(MiniGridEnv):
     def _gen_mission():
         return "get to the goal safely"
 
-    def _gen_grid_(self, width: int, height: int) -> None:
-        # Create an empty grid
-        self.grid = BeliefGrid(width, height)
-
-        # Generate the surrounding walls
-        self.grid.wall_rect(0, 0, self.width, self.height)
-
-        # Blue Channel Threshold to be consider water 
-        threshold = 150
-
-        # Gaussian Blur to deal with labels and smaller streams
-        blur_kernal_size = (21, 21)
-
-        img = self.env_img
-
-        # Trim image to fit x_dim and y_dim and make greyscale based off blue channel 
-        img = img[:(img.shape[0] // self.height) * self.height, :(img.shape[1] // self.width) * self.width, 2]
-
-        # Apply Gaussain Blue
-        img = cv2.GaussianBlur(img, blur_kernal_size, 0)
-        
-        # Make image black or white
-        img = (img > threshold).astype(np.uint8)
-
-        img = img * 255
-
-        # Min Pooling to downscale image
-        img = skimage.measure.block_reduce(img, (int(img.shape[0] / self.height), int(img.shape[1] / self.width)), np.min)
-
-        coordinates_of_land = np.argwhere(img == 255)
-        list_land = list(map(tuple, coordinates_of_land))
-        for tup in list_land: 
-            # Ignore if it already on the border
-            if tup[0] != 0 and tup[0] != height - 1 and tup[1] != 0 and tup[1] != width - 1:
-                self.grid.set(tup[1], tup[0], Lava())
-
-        # If an agent start position is not definied, we will look for an empty spot
-        self.agent_pos = self.agent_start_pos 
-        self.agent_dir = self.agent_start_dir
-
-        if self.goal_pos is not None:
-            self.put_obj(Goal(), self.goal_pos[0], self.goal_pos[1])
-
     def _gen_grid(self, width: int, height: int) -> None: 
         self.grid = BeliefGrid(width, height)
 
