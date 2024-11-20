49ce724b9fcfa31410d1d129e125278e5d731c06
Kasra Torshizi <ktorsh@umd.edu>
2024-11-20 12:13:49 -0500

    Finetune program

--- image_to_grid_env.py
+++ image_to_grid_env.py
@@ -139,7 +139,7 @@ class ImageToGrid(MiniGridEnv):
     def _gen_mission():
         return "get to the goal safely"
 
-    def _gen_grid(self, width: int, height: int) -> None:
+    def _gen_grid_(self, width: int, height: int) -> None:
         # Create an empty grid
         self.grid = BeliefGrid(width, height)
 
@@ -181,11 +181,48 @@ class ImageToGrid(MiniGridEnv):
 
         if self.goal_pos is not None:
             self.put_obj(Goal(), self.goal_pos[0], self.goal_pos[1])
+
+    def _gen_grid(self, width: int, height: int) -> None: 
+        self.grid = BeliefGrid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, self.width, self.height)
+
+        # Blue Channel Threshold to be consider water 
+        blue_color = np.array([253, 183, 147])
+
+        img = self.env_img
+
+        # Trim image to fit make it a multiple of x_dim and y_dim 
+        img = img[:(img.shape[0] // self.height) * self.height, :(img.shape[1] // self.width) * self.width]
+
+        # Discrimate pixels of our desired color
+        img = (img[:, :, 0] == blue_color[0]) & (img[:, :, 1] == blue_color[1]) & (img[:, :, 2] == blue_color[2])
+        img = (img).astype(np.uint8)
+        img = img * 255
+
+        # Min Pooling to downscale image
+        img = skimage.measure.block_reduce(img, (int(img.shape[0] / self.height), int(img.shape[1] / self.width)), np.min)
+
+        coordinates_of_land = np.argwhere(img == 0)
+        list_land = list(map(tuple, coordinates_of_land))
+        for tup in list_land: 
+            # Ignore if it already on the border
+            if tup[0] != 0 and tup[0] != height - 1 and tup[1] != 0 and tup[1] != width - 1:
+                self.grid.set(tup[1], tup[0], Lava())
+
+        # If an agent start position is not definied, we will look for an empty spot
+        self.agent_pos = self.agent_start_pos 
+        self.agent_dir = self.agent_start_dir
+
+        if self.goal_pos is not None:
+            self.put_obj(Goal(), self.goal_pos[0], self.goal_pos[1])
+
     
 # Example Testing Function 
 # Alter or remove this based on what you're planning on doing    
 def main():
-    env = ImageToGrid(cv2.imread("env_images/st_thomas.png"), width = 80, height = 60, render_mode="human")
+    env = ImageToGrid(cv2.imread("env_images/st_thomas_no_labels.png"), width = 80, height = 60, render_mode="human")
     manual_control = ManualControl(env, seed=42)
     manual_control.start()
 if __name__ == "__main__":
